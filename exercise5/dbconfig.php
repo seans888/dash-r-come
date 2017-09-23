ed76a33ef3bb76cce34e5a23d192a53d6082362e
alyssaanne <jayalyfantonalgo@gmail.com>
2017-09-23 09:59:39 -0700

    Testing..

--- exercise5/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$datbase = "test";
-$con = new mysqli($host, $user, $password);
-
-mysqli_select_db($con, $datbase) or die(mysqli_error($con));
-?>
